8e9d3e7f16600bfabb59b59f521b904a84e24082
agandia9 <alejandro.gandia9@gmail.com>
2017-10-09 12:44:12 +0200

    handleclick

--- src/index.js
+++ src/index.js
@@ -7,15 +7,32 @@ import './App.css';
 
 //Create first component using Class
 class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			clicks: 0,
+			name: 'Alex'
+		}
+	}
+//Method handleClick
+	handleClick = () =>{
+  		console.log('click!')
+  		this.setState((prevState)=>{
+  			return {
+  				clicks: prevState.clicks+ 1
+  			}
+  		})
+  	}
   render() {
-  	let myName = 'Alex'
 	return (
 	<div>
-		<h1>Welcome to React, {myName}</h1>
+		<h1>Welcome to React, {this.props.helloTo}</h1>
+		<button onClick={this.handleClick}>click me!</button>
+		<p> clicks => {this.state.clicks}</p>
 	</div>
 	);
   }
 }
 
-//Render with ReactDOM
-ReactDOM.render(<App />, document.getElementById('root'));
+//Render the component with ReactDOM
+ReactDOM.render(<App helloTo="Alex"/>, document.getElementById('root'));
